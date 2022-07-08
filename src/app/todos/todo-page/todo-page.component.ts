1887f28f16eae1648884a82c70326ce79be51c6b
roncor <ronnycortez1988@gmail.com>
2022-07-08 15:10:27 -0600

    Changes on path

--- src/app/todos/todo-page/todo-page.component.ts
+++ src/app/todos/todo-page/todo-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../../../../02-redux-app/src/app/app.reducers';
+import { AppState } from 'src/app/app.reducer';
 import { completados } from '../todo.actions';
 
 @Component({
